ce026afcab064dc884cac61e6ef678e58f4d57d4
Francisco López <flopez_1384@hotmail.com>
2021-02-20 09:13:28 -0600

    [ADD] Basic structure to the App and create de Profile component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
-import { FcAssistant } from "react-icons/fc";
+import Profile from "./Profile";
 
 function App() {
   return (
-    <h1>
-      Hello my friend REACT
-      <FcAssistant />
-    </h1>
+    <main>
+      <section className='container'>
+        <div className='container-title'>
+          <h2>Our Profiles</h2>
+        </div>
+        <Profile />
+      </section>
+    </main>
   );
 }
 
