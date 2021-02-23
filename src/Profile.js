1da653fa6821897e274c06da3093ce99eb1ff4f7
Francisco López <flopez_1384@hotmail.com>
2021-02-23 12:57:57 -0600

    [ADD] Some styles to the App and Profile components, also connect data to the Profile component

--- src/Profile.js
+++ src/Profile.js
@@ -1,7 +1,29 @@
-import React from "react";
-
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import "./profile.css";
 const Profile = () => {
-  return <h2>Hello my friend PROFILE component</h2>;
+  const [index, setIndex] = useState(2);
+  console.log(people);
+  const { name, job, image, text } = people[index];
+
+  return (
+    <article className='profile'>
+      <div className='profile_image'>
+        <img src={image} alt={name} />
+        <span></span>
+      </div>
+      <h4 className='profile_name'>{name}</h4>
+      <p className='profile_title'>{job}</p>
+      <p className='profile_description'>{text}</p>
+      <div>
+        <button>
+          <FaChevronLeft />
+          <FaChevronRight />
+        </button>
+      </div>
+    </article>
+  );
 };
 
 export default Profile;
